f7e0c660b5c2442e28c9666aa79a1d10f785fd85
huangyijie <yijie4@illinois.edu>
2019-11-21 22:56:59 -0600

    Activity1 commit by Yijie Huang version 1 add comments add code add empty imageview 11/21 23.00

--- app/src/main/java/com/example/final2/Game1Activity.java
+++ app/src/main/java/com/example/final2/Game1Activity.java
@@ -2,13 +2,108 @@ package com.example.final2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import static android.content.ContentValues.TAG;
+
+
+/** this game is about Drinking water by sensor in Android.
+ *  Picture should be replaced.
+ *  set a unused button to setText eg. "Drink"
+ *  three pictures: empty and half and full
+ *  textView showing the progress of game
+ *  Hint button Hint text
+ */
 public class Game1Activity extends AppCompatActivity {
+
+    private static final String TAG = "TEST";
+
+    private SensorManager mySensorManager;
+
+    private Sensor myAccelerometer;
+
+    private TestSensorListener mySensorListener;
+
+    private final double accNumber = 9.3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+        setInitialViews();
+        ImageView full = findViewById(R.id.full);
+        ImageView half = findViewById(R.id.half);
+        ImageView empty = findViewById(R.id.empty);
+        full.setVisibility(View.VISIBLE);
+        half.setVisibility(View.GONE);
+        empty.setVisibility(View.GONE);
+        mySensorListener = new TestSensorListener();
+        mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        myAccelerometer = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        // 注册传感器监听函数
+        mySensorManager.registerListener(mySensorListener, myAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 注销监听函数
+        mySensorManager.unregisterListener(mySensorListener);
+    }
+
+    private void setInitialViews() {
+        TextView sensorInfotex = findViewById(R.id.infotext);
+        sensorInfotex.setText("Drinking Water Game");
+    }
+
+    class TestSensorListener implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            // 读取加速度传感器数值，values数组0,1,2分别对应x,y,z轴的加速度
+            Log.i(TAG, "SensorChangedValue: " + event.values[0] + ", " + event.values[1] + ", " + event.values[2]);
+
+            TextView sensorInfotext = findViewById(R.id.infotext);
+            ImageView full = findViewById(R.id.full);
+            ImageView half = findViewById(R.id.half);
+            ImageView empty = findViewById(R.id.empty);
+
+
+            if (half.getVisibility() == View.VISIBLE && event.values[1] < -accNumber
+                    && sensorInfotext.getText().toString().equals("Drink it again")) {
+                sensorInfotext.setText("tada finish");
+                half.setVisibility(View.GONE);
+                empty.setVisibility(View.VISIBLE);
+            }
+
+            if (half.getVisibility() == View.VISIBLE && event.values[1] > accNumber) {
+                sensorInfotext.setText("Drink it again");
+            } else if (event.values[1] < -accNumber && full.getVisibility() == View.VISIBLE) {
+                sensorInfotext.setText("Drink the Water");
+                full.setVisibility(View.GONE);
+                half.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            Log.i(TAG, "onAccuracyChanged");
+        }
     }
 }
