700579d1ae40c8c678e167ecbeb67dabdda954d5
huangyijie <yijie4@illinois.edu>
2019-12-08 14:05:50 -0600

    animation of activity9

--- app/src/main/java/com/example/final2/Game9Activity.java
+++ app/src/main/java/com/example/final2/Game9Activity.java
@@ -2,12 +2,16 @@ package com.example.final2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -20,6 +24,13 @@ public class Game9Activity extends AppCompatActivity {
     private ProgressBar progressBar;
     private int i = 0;
     private TextView currentpercent;
+    private ImageView batteryanimate;
+    private AnimationDrawable battery1;
+    private AnimationDrawable battery2;
+    private AnimationDrawable battery3;
+    private AnimationDrawable battery4;
+    private AnimationDrawable battery5;
+
 
     /** hint button. **/
     private Button hintButton;
@@ -35,6 +46,9 @@ public class Game9Activity extends AppCompatActivity {
         mySensorManager.registerListener(listener, mySensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
         progressBar = findViewById(R.id.progressbar);
+        batteryanimate = findViewById(R.id.nocharge);
+
+
 
         /** hint button listener - please go to the showHint method at the end and add hint **/
         hintButton = findViewById(R.id.hintButton);
@@ -54,10 +68,39 @@ public class Game9Activity extends AppCompatActivity {
     private SensorEventListener listener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
+            Log.e("I", i+"");
+            if (i >=0 && i <= 5) {
+                batteryanimate.setBackgroundResource(R.drawable.activity9animation0);
+                battery1 = (AnimationDrawable) batteryanimate.getBackground();
+                battery1.start();
+            } else if (i > 5 && i <=25) {
+                battery1.stop();
+                batteryanimate.setBackgroundResource(R.drawable.activity9animation25);
+                battery2 = (AnimationDrawable) batteryanimate.getBackground();
+                battery2.start();
+            } else if (i > 25 && i <=50) {
+                battery2.stop();
+                batteryanimate.setBackgroundResource(R.drawable.activity9animation50);
+                battery3 = (AnimationDrawable) batteryanimate.getBackground();
+                battery3.start();
+            } else if (i > 50 && i <=75) {
+                battery3.stop();
+                batteryanimate.setBackgroundResource(R.drawable.activity9animation75);
+                battery4 = (AnimationDrawable) batteryanimate.getBackground();
+                battery4.start();
+            } else {
+                battery4.stop();
+                batteryanimate.setBackgroundResource(R.drawable.activity9animation100);
+                battery5 = (AnimationDrawable) batteryanimate.getBackground();
+                battery5.start();
+            }
+
             if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
                 myLux = sensorEvent.values[0];
                 if (i >= 100) {
                     i = 100;
+                    battery5.stop();
+                    batteryanimate.setImageResource(R.drawable.batteryfull);
                     Pass dialog = new Pass();
                     dialog.levelPassed(9);
                     dialog.show(getSupportFragmentManager(), "Pass");
@@ -66,7 +109,7 @@ public class Game9Activity extends AppCompatActivity {
                 text.setText("Value is: " + myLux);
                 currentpercent.setText(i + "%");
                 progressBar.setProgress(i);
-                if (myLux > 600) {
+                if (myLux > 100) {
                     i = i + 1;
                 }
             }
@@ -80,7 +123,7 @@ public class Game9Activity extends AppCompatActivity {
     /** enter your hint for this level in this method. **/
     private void showHint() {
         Hint dialog = new Hint();
-        dialog.addHint("enter hint here. (e.g. Game6Activity");
+        dialog.addHint("Using the sunlight");
         dialog.show(getSupportFragmentManager(), "Hint");
     }
 }
