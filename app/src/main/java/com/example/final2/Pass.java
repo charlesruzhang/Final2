6aa788948ac82543a160dc9ee5b81d61e1a8eb13
linjialong <linjial2@illinois.edu>
2019-12-09 09:10:12 -0600

    announcement for pass game dialog - updated catalog button * 9 - ver2 game ui - ver 1 pass game dialog - set announcement for hint - updated hint button - set activity 6 - ver 2 activity 5 - ver 1 image button * 9 - set button listener - set GameLevel class * 9 - set

--- app/src/main/java/com/example/final2/Pass.java
+++ app/src/main/java/com/example/final2/Pass.java
@@ -1,22 +1,27 @@
 package com.example.final2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import java.lang.ref.WeakReference;
+
 public class Pass extends DialogFragment {
     private static final String TAG = "Pass";
     private Button returnButton;
     private Button continueButton;
     private int currentLevel;
+
 
     @Nullable
     @Override
