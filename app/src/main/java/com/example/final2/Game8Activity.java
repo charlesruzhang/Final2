5fcf380fc21535722eb8e8034d51b9bf463e313d
linjialong <linjial2@illinois.edu>
2019-12-11 11:30:22 -0600

    announcement for pass game dialog - updated game8 - ver1 game6UI - ver2 game4UI - ver3 game2UI - updated catalog button * 9 - ver3 game ui - ver 1 pass game dialog - set announcement for hint - updated hint button - set activity 6 - ver 2 activity 5 - ver 1 image button * 9 - set button listener - set GameLevel class * 9 - set

--- app/src/main/java/com/example/final2/Game8Activity.java
+++ app/src/main/java/com/example/final2/Game8Activity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.os.CountDownTimer;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class Game8Activity extends AppCompatActivity {
 
     /** hint button. **/
     private Button hintButton;
+    private Button jeff;
+    private TextView times;
+    private int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +24,35 @@ public class Game8Activity extends AppCompatActivity {
         /** hint button listener - please go to the showHint method at the end and add hint **/
         hintButton = findViewById(R.id.hintButton);
         hintButton.setOnClickListener(v -> showHint());
-        Pass dialog = new Pass();
-        dialog.levelPassed(8);
-        dialog.show(getSupportFragmentManager(), "Pass");
+
+        times = findViewById(R.id.times);
+        jeff = findViewById(R.id.jeff);
+        jeff.setOnClickListener(v -> {
+            num = num + 10;
+            times.setText("" + num);
+            test();
+        });
+        jeff.setOnLongClickListener(unused -> {
+            num = num + 5;
+            times.setText("" + num);
+            test();
+            return true;
+        });
+
+    }
+
+    private void test() {
+        if (num == 125) {
+            Pass dialog = new Pass();
+            dialog.levelPassed(8);
+            dialog.show(getSupportFragmentManager(), "Pass");
+        }
     }
 
     /** enter your hint for this level in this method. **/
     private void showHint() {
         Hint dialog = new Hint();
-        dialog.addHint("enter hint here. (e.g. Game6Activity");
+        dialog.addHint("LONG CLICK!");
         dialog.show(getSupportFragmentManager(), "Hint");
     }
 }
